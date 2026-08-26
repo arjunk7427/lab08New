c779434efc9272cde5e865b9ee6933fd4362a5a1
Arjun Kaondal <arjunk7427@gmail.com>
2026-08-26 17:58:29 +0000

    changed main

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -2,7 +2,7 @@ package com.neueda.leap;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("hello world from the team's Sprint 1 project skeleton");
+        System.out.println("hhello world from the team's Sprint 1 project skeleton");
         System.out.println("Container is up. Sleeping so you can inspect it.");
         Thread.sleep(600_000);
     }
